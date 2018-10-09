3857bc496c879f972deb14f903744f5943b9b868
cyncx <30388906+cyncx@users.noreply.github.com>
2018-10-09 22:31:05 +0100

    fixed errors
    put header files in the wrong order

--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -1,7 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Runtime/Engine/Classes/Engine/World.h"
 #include "TankBarrel.h"
+#include "Runtime/Engine/Classes/Engine/World.h"
+
 
 
 void UTankBarrel::ElevateBarrel(float RelativeSpeed)
